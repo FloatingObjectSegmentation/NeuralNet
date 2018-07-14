6c9cd1937c7a295a8cd112db7cbc97a8457bfead
kristijan.mirceta <kristijan.mirceta@gmail.com>
2018-07-14 17:09:56 +0200

    Some

--- start_test/kerastrain.py
+++ start_test/kerastrain.py
@@ -3,17 +3,40 @@ from keras.models import Sequential
 from keras.layers import Dense, Flatten
 from keras.layers import Conv3D, MaxPooling3D
 from keras.optimizers import SGD
+import scipy.io
+import os
+
 
 # Generate dummy data
-x_train = np.random.random((100, 32, 32, 32, 1))
-y_train = np.random.randint(2, size=(100, 1))
-x_test = np.random.random((20, 32, 32, 32, 1))
-y_test = np.random.randint(2, size=(20, 1))
+x_train = np.random.random((3700, 50, 50, 50, 1))
+y_train = np.random.randint(2, size=(3700, 1))
 
+# Load real shit data
+
+folder = '/Users/km/Desktop/playground/mag/magneuron/result/'
+for i in range(3700):
+    A = scipy.io.loadmat(folder + 'examples/' + str(i) + '.mat')
+    A = A['objectField']
+    x_train[i,:,:,:,0] = A
+
+    y = scipy.io.loadmat(folder + 'targets/' + str(i) + '.mat')
+    y = y['target']
+    y = 1 if y == 1 else 0 # DRONE IS 1, OTHER IS 0
+    y_train[i] = y
+
+    if i % 100 == 0:
+        print('Loaded ' + str(i) + ' examples')
+
+x_test = x_train[2700:]
+y_test = y_train[2700:]
+x_train = x_train[:2700]
+y_train = y_train[:2700]
+
+# network
 model = Sequential()
 model.add(Conv3D(filters=32, kernel_size=(5,5,5), strides=(2,2,2),
                  padding='same', data_format='channels_last', use_bias=True,
-                 input_shape=(32,32,32,1)))
+                 input_shape=(50,50,50,1)))
 model.add(Conv3D(filters=32, kernel_size=(3,3,3),
                  padding='same', data_format='channels_last', use_bias=True))
 # Valid question: Should I use stride during maxpooling?
